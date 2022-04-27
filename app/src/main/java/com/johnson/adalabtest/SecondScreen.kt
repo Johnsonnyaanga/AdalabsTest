376ebb1ae231d40156069559c69fd579d9499af5
unknown <johnsonyaanga@gmail.com>
2022-04-27 10:15:33 +0300

    refine UI

--- app/src/main/java/com/johnson/adalabtest/SecondScreen.kt
+++ app/src/main/java/com/johnson/adalabtest/SecondScreen.kt
@@ -3,10 +3,10 @@ package com.johnson.adalabtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SecondScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second_screen)
         supportActionBar?.hide()
     }
 }
